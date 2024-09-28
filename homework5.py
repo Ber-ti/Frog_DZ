5c1cc2b65421ea0c305464020a1b3ae5975f7547
Ber-ti <prorok443321@yandex.ru>
2024-09-28 18:27:32 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,20 @@
+#2
+
+print('[2]:')
+my_list=['apple','banana','orange','pear','mango','lemon']
+print(my_list)
+print(my_list[0],',',my_list[-1])
+print(my_list[2:5])
+my_list[2]='sas'
+print(my_list)
+
+#3
+
+print('[3]:')
+my_dick={'little': 'маленький', 'red':'красный', 'gnome': 'гном'}
+print(my_dick)
+print(my_dick.get('little'))
+my_dick['little']='крошечный'
+my_dick.setdefault('fishing','рыбачит')
+print(my_dick)
+
