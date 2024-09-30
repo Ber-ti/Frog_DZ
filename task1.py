ba3d1ddb213e42db74921cb58b26dad20e5dd500
Ber-ti <prorok443321@yandex.ru>
2024-09-30 13:36:11 +0500

    task1.py

---
+++ task1.py
@@ -0,0 +1,17 @@
+print(12+89-1)
+
+#1st program
+print(9**0.5)
+#2nd program
+print(9.99>9.98,1000!=1000.1)
+#3rd program
+print(2*2+2,2*(2+2))
+print(2*2+2==2*(2+2),2*2+2!=2*(2+2),2*2+2>2*(2+2))
+#4th program
+x='123.456'
+print(x[4])
+x2=float(x)
+print(type(x2))
+print(x2*10)
+x3=int(x2)
+print(x3,'|',x3/10)
